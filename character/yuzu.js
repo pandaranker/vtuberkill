7a6fc28ba736c1a8b01530c79f973c3a4486d752
yuzu <1540008331@qq.com>
2020-11-01 19:55:58 +0800

    测试人物

---
+++ character/yuzu.js
@@ -0,0 +1,359 @@
+'use strict';
+
+
+game.import('character',function(lib,game,ui,get,ai,_status){
+	return {
+		name:"yuzu",
+		connectBanned:['NekomiyaHinata','SisterClearie'],
+		connect:true,
+		character:{
+			NekomiyaHinata:['female','qun',3,['yuchong', 'songzang', 'zhimao']],
+			SisterClearie:['female','nijisanji',3,['zhenxin','zhuwei']],
+		},
+		characterIntro:{
+			NekomiyaHinata: '“这不是猫耳，这是头发啦！”',
+			SisterClearie:	'“今日也愿神加护于你……”',
+		},
+		skill:{
+			//猫宫
+			yuchong:{
+			//	group:['yuchong_dist' , 'yuchong_uneq'],
+			//	subSkill: 
+			//	{			
+			//		dist:{			
+						mod:{
+								//距离变化
+								attackFrom:function(from,to,distance){
+									if(from.getEquip(1))
+									{
+										return distance-1; 
+									}                
+								},
+								globalTo:function(from,to,distance){
+									if(to.getEquip(1))
+									{
+										return distance+1;
+									}
+								},
+								//无法弃置
+								canBeDiscarded:function(card,player,target,name,now){
+									if(get.subtype(card)=='equip1')
+									{
+										return false;
+									}
+								},
+							}
+						
+			//		},
+					//无视防具
+					/*uneq:{
+						
+						trigger:{player:'useCardToPlayered'},
+						forced:true,
+						priority:9,
+						filter:function(event,player){
+							if(!player.getEquip(1))		return false;
+							return event.card.name=='sha'||get.type(event.card)=='trick';
+						},
+						logTarget:'target',
+						content:function(){
+								player.addTempSkill('unequip');
+						},
+					}*/
+			//	}
+			},										
+			songzang:{
+				trigger:{player:'useCardToPlayered'},
+				priority:8,
+				filter:function(event,player){
+					return event.card.name=='sha'&&!(event.target.maxHp/2 < event.target.hp)&&player.countCards('he')>0;
+				},
+				logTarget:'target',
+				content:function(){
+					'step 0'
+					player.chooseToDiscard(get.prompt('songzang'),'he');
+							
+					'step 1'	//无法被响应	
+					//trigger.getParent().directHit.add(trigger.target);
+								//伤害+1
+					trigger.getParent().baseDamage++;
+					trigger.target.addTempSkill('songzang2');
+					trigger.target.addTempSkill('songzang4');
+					trigger.target.storage.songzang2.add(trigger.card);
+				},
+				ai:{
+					effect:{
+						target:function(card,player,target,current){
+							if(card.name=='sha'&&current<0) return 0.7;
+						}
+					}
+				}
+			},
+			songzang2:{
+				firstDo:true,
+				ai:{unequip2:true},
+				init:function(player,skill){
+					if(!player.storage[skill]) player.storage[skill]=[];
+				},
+				onremove:true,
+				trigger:{
+					player:['damage','damageCancelled','damageZero'],
+					target:['shaMiss','useCardToExcluded'],
+				},
+				charlotte:true,
+				filter:function(event,player){
+					return player.storage.songzang2&&event.card&&player.storage.songzang2.contains(event.card);
+				},
+				silent:true,
+				forced:true,
+				popup:false,
+				priority:12,
+				content:function(){
+					player.storage.songzang2.remove(trigger.card);
+					if(!player.storage.songzang2.length) player.removeSkill('songzang2');
+				},
+			},
+			songzang3:{
+				mod:{
+					cardSavable:function(card){
+						if(card.name=='tao') return false;
+					},
+				},
+			},
+			songzang4:{
+				trigger:{player:'dyingBegin'},
+				forced:true,
+				silent:true,
+				firstDo:true,
+				filter:function(event,player){
+					return event.getParent(2).songzang_buffed=true;
+				},
+				content:function(){
+					player.addTempSkill('songzang3',{global:['dyingEnd','phaseEnd']});
+				},
+			},
+			zhimao:{
+				trigger:{target:'useCardToBegin'},
+				forced:true,
+				priority:15,
+				filter:function(event,player){
+					if(!event.target||event.target!=player||event.player==player) return false;
+					if(player.inRange(event.player)) return false; 	//使用牌者在攻击距离外
+					return (get.type(event.card)=='trick');		//牌为锦囊牌
+				},
+				content:function(){
+					'step 0'
+					var target = trigger.player;			
+					if(target.getEquip(1)){
+						player.chooseControlList(
+							['取消之并抽一张牌',
+							'获得'+get.translation(target)+'的武器牌，视为对其使用【杀】'],
+							true,function(event,player){
+							return _status.event.index;
+						});
+					}
+					else{
+			//			player.chooseControlList(
+			//				['抽一张牌',
+			//				'视为对其使用【杀】'],
+			//				true,function(event,player){
+			//				return _status.event.index;
+			//			});
+						player.draw();
+						trigger.cancel();
+					}
+					'step 1'
+					var target = trigger.player;				
+			//			if(target.getEquip(1)){
+							if(result.index==1){
+								player.line(target);
+								player.gain(target.getEquip(1),target,'give','bySelf');
+								player.useCard({name:'sha',isCard:true},target).animate=false;
+							}	
+							else{
+								player.draw();
+								trigger.cancel();
+							}						
+			//			}
+			//			else{
+			//				if(result.index==1){
+			//					player.line(target);
+			//					player.useCard({name:'sha',isCard:true},target).animate=false;
+			//				}	
+			//				else						player.draw();
+							
+			//			}
+					
+			//		'step2'
+			//		trigger.cancel();
+				}									
+				
+			},
+			//修女
+			zhenxin:{
+				group:['zhenxin_from' , 'zhenxin_to'],
+				subSkill:{
+					//防止每回合你第一次对体力值小于你的角色造成的伤害
+					from:{
+						trigger:{source: 'damageBefore' },
+						forced:true,
+						usable:1,
+						priority:12,
+						filter:function(event,player){
+							console.log(event);
+							if(event.player==player)	return false;
+							return player.hp>event.player.hp;
+						},
+						content:function(){
+							console.log(trigger.getParent());
+							trigger.num--;
+						},
+					},
+					//防止体力值大于你的角色每回合对你造成的第一次伤害
+					to:{
+						trigger:{player: 'damageBefore' },
+						forced:true,
+						usable:1,
+						priority:24,
+						filter:function(event,player){
+							if(event.player!=player)	return false;
+							return player.hp<event.source.hp;
+						},
+						content:function(){
+							trigger.num--;
+						},
+					}
+				}
+			},
+			zhuwei:{
+				global:'zhuwei_put',
+				group:['zhuwei_moveC'],
+				subSkill:{
+					put:{
+						trigger:{player:'phaseEnd'},
+						forced:false,
+						check: function(event, player) {
+							var target = game.findPlayer(function(cur) {
+								return cur.hasSkill('zhuwei');
+							});
+							return target && get.attitude(player, target) > 0;
+						},
+						filter:function(event,player){
+							if(player.hasSkill('zhuwei'))		return false;
+							return	!game.hasPlayer(function(cur) {
+								return cur.countCards('h') < event.player.countCards('h');
+							});
+						},
+						content:function(){
+							player.draw();
+							var target = game.findPlayer(function(cur) {
+								return cur.hasSkill('zhuwei');
+							});
+							target.draw();
+						},
+					},
+					moveC:{
+						trigger:{global:'zhuwei_putAfter'},
+						forced:false,
+						filter:function(event,player){
+							console.log(event);
+							return player.countCards('e')||event.player.countCards('e');
+						},	
+						content:function(){
+							'step 0'
+							console.log(this.trigger);
+							next=player.chooseTarget(2,function(card,player,target){
+								if(ui.selected.targets.length){
+									var from=ui.selected.targets[0];
+									if(target.isMin()) return false;
+									if(from==trigger.player&&player!=target)		return false;
+									if(from==player&&trigger.player!=target)		return false;
+									var es=from.getCards('e');
+									for(var i=0;i<es.length;i++){
+										if(target.isEmpty(get.subtype(es[i]))) return true;
+										return true;
+									}
+									return false;
+								}
+								else{
+									return (target== player||target== trigger.player)&&target.countCards('e')>0;
+								}
+							});
+							/*next.set('filterButton',function(button){
+								if(event.customFilterButton){
+									if (!event.customFilterButton(button))
+										return false;
+								}}
+							);*/
+							next.set('multitarget',true);
+							next.set('targetprompt',['被移走','移动目标']);
+							next.set('prompt',event.prompt||'移动场上的一张装备牌');
+							next.set('forced',true);
+							'step 1'
+							if(result.bool){
+								player.line2(result.targets,'green');
+								event.targets=result.targets;
+							}
+							else{
+								event.finish();
+							}
+							'step 2'
+							game.delay();
+							'step 3'
+							if(targets.length==2){
+								player.choosePlayerCard('e',true,function(button){
+									var player=_status.event.player;
+									var targets0=_status.event.targets0;
+									var targets1=_status.event.targets1;
+									if(get.attitude(player,targets0)>get.attitude(player,targets1)){
+										if(get.value(button.link,targets0)<0) return 10;
+										return 0;
+									}
+									else{
+										return get.equipValue(button.link);
+									}
+								},targets[0]).set('targets0',targets[0]).set('targets1',targets[1]).set('filterButton',function(button){
+									var targets1=_status.event.targets1;
+									return targets1.isEmpty(get.subtype(button.link));
+								});
+							}
+							else{
+								event.finish();
+							}
+							'step 4'
+							if(result.bool&&result.links.length){
+								var link=result.links[0];
+								event.targets[1].equip(link);
+								event.targets[0].$give(link,event.targets[1]);
+								game.delay();
+								event.result={bool:true};
+							}
+						}
+					}
+				},
+				
+				
+				
+			}
+			
+			
+				
+			
+		}, 
+		translate:{
+			NekomiyaHinata:'猫宫日向',
+			yuchong: '一命通关',
+			yuchong_info: '锁定技。你装备区内的武器牌不能被弃置。你在装备武器时，其他角色计算与你的距离+1，你的攻击距离+1。',
+			songzang: '送葬天使',
+			songzang_info: '你使用【杀】指定已损失体力值超过体力上限一半的角色为目标时，你可以弃一张牌令此【杀】伤害+1，若其因此【杀】的伤害而进入濒死状态，则其不能使用【桃】直到此濒死事件结算。',
+			zhimao: '只箱只猫',
+			zhimao_info: '当你成为非延时性锦囊牌的目标时，若来源在你攻击范围外，你可选择一项：取消之并摸一张牌；获得其武器牌，视为向其使用一张无距离限制的【杀】。',
+			SisterClearie:'修女·克蕾雅',
+			zhenxin: '真信之诚',
+			zhenxin_info: '锁定技。防止每回合你第一次对体力值小于你的角色造成的伤害；防止体力值大于你的角色每回合对你造成的第一次伤害。',
+			zhuwei: '助危之心',
+			zhuwei_info: '其他角色的结束阶段，若其手牌为全场最少，其可以与你各摸一张牌，然后你可以移动你或其装备区的一张牌。',
+			
+		},
+	};
+});
