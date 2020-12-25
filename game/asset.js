727f8a24cb166139fddbb2830231e8390f349753
yuzu <1540008331@qq.com>
2020-12-25 22:06:57 +0800

    。05

--- game/asset.js
+++ game/asset.js
@@ -690,13 +690,7 @@ window.noname_asset_list=[
 	
 	'image/replace/chuqibuyi.png',
 	'image/replace/dongzhuxianji.png',
-	'image/replace/heiguangkai.png',
 	'image/replace/suijiyingbian.png',
-	'image/replace/taigongyinfu.png',
-	'image/replace/tianjitu.png',
-	'image/replace/tongque.png',
-	'image/replace/wutiesuolian.png',
-	'image/replace/wuxinghelingshan.png',
 	'image/replace/zhujinqiyuan.png',
 	
 	'image/character/AkaiHaato.jpg',
